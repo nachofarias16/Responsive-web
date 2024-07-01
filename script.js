9dc3378910c898b1c3752e4a3cd8c724318fc887
Nacho <nachofarias16@gmail.com>
2024-07-01 12:57:46 -0300

    animacion de navbar desaperece y aparece ademas de agregado de comentarios

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
-const text = "Programador y desarrollador";
-const speed = 85; 
-let i = 0;
+//texto que aparece
+const text = "Programador y desarrollador"; //tomo el texto que quiero que aparezca
+const speed = 85; //defino la cantidad de tiempo que tarda en aparecer
+let i = 0; 
 
 function typeWriter() {
-  if (i < text.length) {
-    document.querySelector('h3').innerHTML += text.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+  if (i < text.length) { //hasta que i alcance el valor del tamaño total de texto
+    document.querySelector('h3').innerHTML += text.charAt(i); //tomo el h3 y le sumo el caracter de la posicion i
+    i++; //agrego +1 al valor de i
+    setTimeout(typeWriter, speed); //llamo a la funcion de nuevo despues del tiempo que estableci antes
   }
 }
+window.onload = typeWriter; //defino que esto ocurra cuando cargo la pagina
 
-window.onload = typeWriter;
 
-const button = document.querySelector('.contactar');
+// Movimiento del botón de contacto
+const button = document.querySelector('.contactar'); //tomo el boton de clase contactar
 
-button.addEventListener('mousemove', (e) => {
-    const rect = button.getBoundingClientRect();
-    const x = e.clientX - rect.left - rect.width / 2;
+button.addEventListener('mousemove', (e) => { //event listener a cuando se mueve el mouse sobre el boton
+    const rect = button.getBoundingClientRect(); //consigo las dimensiones del boton
+    const x = e.clientX - rect.left - rect.width / 2; //calculo posicion x y del boton relativo a su centro
     const y = e.clientY - rect.top - rect.height / 2;
-    button.style.transform = `scale(1.1) translate(${x * 0.3}px, ${y * 0.3}px)`;
+    button.style.transform = `scale(1.2) translate(${x * 0.3}px, ${y * 0.3}px)`; //cambio el tamaño del boton y lo muevo basado en la pos del mouse
 });
 
-button.addEventListener('mouseleave', () => {
-    button.style.transform = 'scale(1)';
+button.addEventListener('mouseleave', () => {  //agrego event listener a cuando el mouse abandona el boton
+    button.style.transform = 'scale(1)';  //lo devuelvo a sus dimensiones normales
+});
+
+//navbar desapareciendo y apareciendo 
+const navbar = document.querySelector('.navbar-section'); //tomo el elemento con la clase navbar-section
+let lastScrollY = window.scrollY; //tomo la posicion del scrolleo actual
+let ticking = false; //inicializo una variable en false para controlar si hay actualizaciones de scroll
+
+function handleScroll() {
+  if (lastScrollY < window.scrollY) {  //verifico si scroll actual es mayor al ultimo (va hacia abajo)
+    //si esto ocurre oculto la barra añadiendo a su clase navbar-hide
+    navbar.classList.add('navbar-hide');
+  } else {
+    //caso contrario remuevo esa clase y se muestra
+    navbar.classList.remove('navbar-hide');
+  }
+  //actualizo la posicion de scroll
+  lastScrollY = window.scrollY;
+  //reseteo la variable de control
+  ticking = false;
+}
+//agrego event listener que se activa cuando se scrollea
+window.addEventListener('scroll', () => {
+    //si no hay actualizaciones en la variable
+  if (!ticking) {
+    //uso request animation frame para llamar a handle scroll y mejorar rendimiento
+    window.requestAnimationFrame(handleScroll);
+    //indico que hay scroll en curso
+    ticking = true;
+  }
 });
