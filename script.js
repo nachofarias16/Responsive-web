0f01eb0fec34d7e1e3b9b014590bdb9f10cc7522
Nacho <nachofarias16@gmail.com>
2024-07-01 11:41:39 -0300

    animacion a boton de contactar y baje el texto de la navbar

--- script.js
+++ script.js
@@ -12,4 +12,15 @@ function typeWriter() {
 
 window.onload = typeWriter;
 
+const button = document.querySelector('.contactar');
 
+button.addEventListener('mousemove', (e) => {
+    const rect = button.getBoundingClientRect();
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
+    button.style.transform = `scale(1.1) translate(${x * 0.3}px, ${y * 0.3}px)`;
+});
+
+button.addEventListener('mouseleave', () => {
+    button.style.transform = 'scale(1)';
+});
